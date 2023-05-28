5ffb5cdfdfa339840aacb1b6fb1da1242b4a1129
Hanieh Mahdavi <haniehmahdavi26@gmail.com>
2023-05-28 17:54:37 +0330

    add `VirgoolAPI` class

---
+++ apis.py
@@ -0,0 +1,11 @@
+from requests import Session
+
+
+class VirgoolAPI:
+    def __init__(self) -> None:
+        self.session = Session()
+        self.session.headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/113.0",
+            "Accept": "*/*",
+            "Accept-Encoding": "gzip, deflate, br",
+        }
